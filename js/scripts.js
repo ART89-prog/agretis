93bf6ec38fc38c733ceef78a3c0066e195ee43fa
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-09-28 15:58:19 +0300

    Доделал

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(() => {
 
-    $('.btn').on('click', function() {
+    $('.header_menu li a').on('click', function() {
+        let href = $(this).attr('href');    
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, {
+            duration: 870,  
+            easing: "linear" 
+        });
+    
+        return false;
+    });
+
+    $('.footer_menu li a').on('click', function() {
         let href = $(this).attr('href');    
         $('html, body').animate({
             scrollTop: $(href).offset().top

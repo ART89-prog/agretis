102bddaf249412e0c494e6954ab6b4f329092250
ART89-prog <dykpgilkom@mail.ru>
2022-09-28 18:57:41 +0300

    Правки

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,19 @@ $(() => {
     });
 
 
+    $('.header-info a').on('click', function() {
+        let href = $(this).attr('href');    
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, {
+            duration: 870,  
+            easing: "linear" 
+        });
+    
+        return false;
+    });
+
+
 
 
 
